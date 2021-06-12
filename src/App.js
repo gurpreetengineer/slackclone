c49b86b85f9ca500c2baaef1075577729016efb1
gurpreetreact <gurpreet.reactdev@gmail.com>
2021-06-12 22:40:55 +0530

    Sidebar Addition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/userInterface/Header';
+import Sidebar from './components/userInterface/Sidebar';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       {/* Header */}
       <Header />
       <div className="" >
-        
+        <Sidebar />
       </div>
       {/* Sidebar */}
       
